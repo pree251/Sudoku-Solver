f16b36dcc9e597a44759cf56d1cb78488e4dc69e
shivratna-kps2019 <shivratna.bhel@gmail.com>
2022-07-30 21:18:00 +0530

    Python script update
    Completed implementation of Sudoku Solver with sample grid

--- Python scripts/Num_possiblity_check.py
+++ Python scripts/Num_possiblity_check.py
@@ -1,15 +1,17 @@
+import numpy as np
+import grid
 def possible(y,x,n):
     global grid
     for i in range(0,9):
-        if grid[y][i]==n:
+        if np.grid[y][i]==n:
             return False
     for i in range(0,9):
-        if grid[i][x]==n:
+        if np.grid[i][x]==n:
             return False
     x0 = (x//3)*3
     y0 = (y//3)*3
     for i in range(0,3):
         for j in range(0,3):
-            if grid[i+y0][j+x0]==n:
+            if np.grid[i+y0][j+x0]==n:
                 return False
     return True
